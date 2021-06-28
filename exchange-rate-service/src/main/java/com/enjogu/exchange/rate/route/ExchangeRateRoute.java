dfc54deba6b6b5357f5d25ab97fa70ed66a59290
eric.njogu <eric.njogu@bringglobal.com>
2021-06-28 17:16:36 +0300

    fix failing integration test
    - add api key prop
    - setup camel marshalling
    - add JWT token to header

--- exchange-rate-service/src/main/java/com/enjogu/exchange/rate/route/ExchangeRateRoute.java
+++ exchange-rate-service/src/main/java/com/enjogu/exchange/rate/route/ExchangeRateRoute.java
@@ -4,6 +4,8 @@ import com.enjogu.exchange.rate.config.EndpointProperties;
 import lombok.RequiredArgsConstructor;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +13,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class ExchangeRateRoute extends RouteBuilder {
   private final EndpointProperties exchangeRateProps;
+  private final String logString = "log:" + getClass() + "?multiline=true&showAll=true";
+  @Value("${wise.api-key}")
+  private String apiKey;
+
   @Override
   public void configure() {
     //@formatter:off
@@ -18,11 +24,15 @@ public class ExchangeRateRoute extends RouteBuilder {
       .routeId(ExchangeRateProxy.ROUTE_ID)
       .setHeader(Exchange.HTTP_METHOD, constant(exchangeRateProps.getHttpMethod()))
       .setHeader(Exchange.CONTENT_TYPE, constant(MediaType.APPLICATION_JSON_VALUE))
+      .setHeader("Authorization", constant(String.format("Bearer %s", apiKey)))
+      .marshal().json(JsonLibrary.Jackson)
+      .to(logString)
       .toD(String.format("%s?source=%s&target=%s&from=%s&to=%s",
           exchangeRateProps.getUrl(), "${exchangeProperty.requestDto.source}",
-          "${exchangeProperty.requestDto.target}", "${exchangeProperty.requestDto.from}",
-          "${exchangeProperty.requestDto.to}"))
-      .unmarshal();
+          "${exchangeProperty.requestDto.target}", "${exchangeProperty.requestDto.dateFrom}",
+          "${exchangeProperty.requestDto.dateTo}"))
+      .unmarshal().json(JsonLibrary.Jackson)
+      .to(logString);
     //@formatter:on
   }
 }
--- exchange-rate-service/src/test/java/com/enjogu/exchange/rate/rest/ExchangeRateControllerTestIT.java
+++ exchange-rate-service/src/test/java/com/enjogu/exchange/rate/rest/ExchangeRateControllerTestIT.java
@@ -15,12 +15,20 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.nio.charset.StandardCharsets;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @IntegrationTest
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 class ExchangeRateControllerTestIT {
   private final MockMvc mvc;
+
+  public static final String TEST_JWT =
+      "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxk"
+          + "ZXIiLCJpYXQiOjE0ODQ4MjAxOTYsImV4cCI6MTUxNjM1NjE5NiwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJv"
+          + "Y2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJFbWFpbCI6Impyb2Nr"
+          + "ZXRAZXhhbXBsZS5jb20iLCJSb2xlIjpbIk1hbmFnZXIiLCJQcm9qZWN0IEFkbWluaXN0cmF0b3IiXSwiaW51aWQiOiJKaW1te"
+          + "SJ9.O9TE28ygrHmDjItYK6wRis6wELD5Wtpi6ekeYfR1WqM";
 
   @BeforeEach
   void setUp() {
@@ -35,20 +43,23 @@ class ExchangeRateControllerTestIT {
 
     String content = IOUtils.resourceToString("/json/response/transferwise-exchange-rate.json",
         StandardCharsets.UTF_8);
-    stubFor(get("/v1/rates")
+
+    stubFor(get(urlMatching("/v1/rates.*"))
         .withQueryParam("from", equalTo(String.format("%sT00:00:00", from)))
-        .withQueryParam("to", equalTo(String.format("%sT23:59:59", to)))
+        .withQueryParam("to", matching(String.format("%sT23:59:59.*", to)))
         .withQueryParam("source", equalTo(source))
         .withQueryParam("target", equalTo(target))
-        .withHeader("Authorization", equalTo(String.format("Bearer %s", "test-token")))
+        .withHeader("Authorization", equalTo(String.format("Bearer %s", "test-api-token")))
         .willReturn(status(HttpStatus.OK.value()).withBody(content))
     );
 
     mvc.perform(MockMvcRequestBuilders.get(
-        "/client-api/v1/exchange-rates?from={from}to={to}&source={source}&target={target}",
+        "/client-api/v1/exchange-rates?from={from}&to={to}&source={source}&target={target}",
         from, to, source, target)
         .accept(MediaType.APPLICATION_JSON)
+        .header("Authorization", TEST_JWT)
     ).andExpect(MockMvcResultMatchers.status().isOk())
-        .andExpect(content().json(content));
+        .andExpect(content().json(content))
+        .andDo(print());
   }
 }
