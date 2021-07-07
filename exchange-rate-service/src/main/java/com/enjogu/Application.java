8659d439440640832d675335d1e9e79f3c4177d1
eric.njogu <eric.njogu@bringglobal.com>
2021-07-07 11:02:39 +0300

    wip: troubleshooting missing *.yml configs

--- exchange-rate-service/src/main/java/com/enjogu/Application.java
+++ exchange-rate-service/src/main/java/com/enjogu/Application.java
@@ -1,16 +1,13 @@
 package com.enjogu;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class Application extends SpringBootServletInitializer {
-
+public class Application {
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
